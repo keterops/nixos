02d01f38c6c143e208fda1d87218089b3e032ab9
sinos31 <zaaain@protonmail.com>
2022-04-12 22:59:21 +0500

    complete nix support in vsc

--- hm/hm.nix
+++ hm/hm.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ git nixfmt ungoogled-chromium feh mpv pantheon.elementary-files pantheon.elementary-photos pantheon.elementary-terminal nixfmt  ];
   programs.home-manager.enable = true;
-  # programs.vscode = {
-  #   enable = true;
-  #   extensions = with pkgs.vscode-extensions; [
-  #     jnoortheen.nix-ide
-  #   ];
-  # };
+  programs.vscode = {
+    enable = true;
+    mutableExtensionsDir = false;
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+    ];
+    userSettings = {
+      "nix.enableLanguageServer" = true;
+    };
+  };
 }
